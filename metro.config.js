41b84276bd506a065a154c6421bba5c2bc8b7078
Dhani5703 <dems9811@naver.com>
2025-09-17 18:13:37 +0900

    feat(RN): @growgrammers/auth-core 패키지 공유용으로 변경

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,4 @@
 // metro.config.js
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-const path = require('path');
+const { getDefaultConfig } = require('@react-native/metro-config');
 
-const projectRoot = __dirname;
-
-const custom = {
-  resolver: {
-    // yalc(=symlink) 패키지 추적
-    unstable_enableSymlinks: true,
-
-    // React/React Native 이중 설치 방지: 오직 현재 앱의 node_modules만 본다
-    nodeModulesPaths: [path.resolve(projectRoot, 'node_modules')],
-  },
-  // 특별한 경우 아니면 transformer/other 옵션 불필요
-};
-
-module.exports = mergeConfig(getDefaultConfig(projectRoot), custom);
+module.exports = getDefaultConfig(__dirname);
